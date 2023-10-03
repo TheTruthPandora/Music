02ddc8268398e062e6fa285bc3a4288b99d9c993
Error1337 <129363912+TheTruthPandora@users.noreply.github.com>
2023-10-03 15:49:36 +0800

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+// 检测窗口大小变化
+window.addEventListener('resize', scaleUI);
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello, Mobile Frontend!</Text>
-    </View>
-  );
-};
+// 初始化时执行一次缩放
+scaleUI();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5f5f5',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333',
-  },
-});
+// 缩放UI
+function scaleUI() {
+  // 获取窗口宽度和高度
+  var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
-export default App;
+  // 设置缩放比例，这里以窗口宽度作为参考
+  var scale = windowWidth / 1920; // 假设设计稿宽度为1920px
+
+  // 获取需要缩放的元素
+  var elementsToScale = document.querySelectorAll('.autoscale');
+
+  // 遍历元素并应用缩放
+  for (var i = 0; i < elementsToScale.length; i++) {
+    var element = elementsToScale[i];
+    element.style.transform = 'scale(' + scale + ')';
+    element.style.transformOrigin = 'top left';
+  }
+}
